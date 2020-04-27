c9bf0889f050c85d6ab5750f809af1d8f2770797
Bryan Berger <bryan@bryanberger.com>
2020-04-26 22:39:15 -0400

    update migration

--- api/src/migrations/20200426230405-create-br-all.js
+++ api/src/migrations/20200426230405-create-br-all.js
@@ -60,10 +60,6 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE,
       },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
     })
   },
   down: (queryInterface, Sequelize) => {
