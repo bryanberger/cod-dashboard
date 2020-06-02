bc07b749f8d7f28f087239d741e40de0d60b78d8
Bryan Berger <bryan@bryanberger.com>
2020-06-02 15:38:00 -0400

    console log on prod too

--- api/src/logger.js
+++ api/src/logger.js
@@ -28,12 +28,12 @@ const logger = createLogger({
 // If we're not in production then log to the `console` with the format:
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
 //
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(
-    new transports.Console({
-      format: format.simple(),
-    }),
-  )
-}
+// if (process.env.NODE_ENV !== 'production') {
+logger.add(
+  new transports.Console({
+    format: format.simple(),
+  }),
+)
+// }
 
 module.exports = logger
