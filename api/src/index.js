5a5f3467623ba3f19fbe0a28b98a1293ce3e4eed
Bryan Berger <bryan@bryanberger.com>
2020-06-16 13:22:55 -0400

    - use npm package for call-of-duty-api - rewrite the MW stat request to include the username and check it

--- api/src/index.js
+++ api/src/index.js
@@ -2,7 +2,7 @@ const db = require('./models')
 const logger = require('./logger')
 const API = require('call-of-duty-api')({
   platform: 'battle',
-  ratelimit: { maxRequests: 1, perMilliseconds: 3000 },
+  ratelimit: { maxRequests: 1, perMilliseconds: 5000 },
 })
 const gamerTags = require('./gamerTags.json')
 const LOOP_TIMEOUT = 15 * 60 * 1000
@@ -25,31 +25,55 @@ const init = async () => {
   }
 }
 
+const CustomMWRequest = (gamertag, platform = 'battle') => {
+  return new Promise((resolve, reject) => {
+    brDetails = {}
+    API.MWwz(gamertag, platform)
+      .then((data) => {
+        let lifetime = data.lifetime
+        if (typeof lifetime !== 'undefined') {
+          let filtered = Object.keys(lifetime.mode)
+            .filter((x) => x.startsWith('br'))
+            .reduce((obj, key) => {
+              obj[key] = lifetime.mode[key]
+              return obj
+            }, {})
+          if (typeof filtered.br_all !== 'undefined') {
+            filtered.br_all.properties.title = 'br_all'
+            filtered.br_all.properties.gamerTag = data.username
+            // flatten down, remove need for 'br_all' sub-object
+            brDetails = filtered.br_all.properties
+          }
+        }
+        resolve(brDetails)
+      })
+      .catch((e) => reject(e))
+  })
+}
+
 const mapGamerTags = async () => {
-  const bulk = []
-  await Promise.all(
+  const bulk = await Promise.all(
     gamerTags.map(async (gamerTag) => {
       try {
-        const wz = await API.MWwz(gamerTag)
-        if (wz.hasOwnProperty('br_all')) {
-          logger.info(`[COD API] Got data for: ${gamerTag}`)
-          if (
-            Object.keys(wz.br_all).length === 0 &&
-            obj.constructor === Object
-          ) {
-            logger.error(`[COD API] Data was null for: ${gamerTag}`)
-            return
+        const wz = await CustomMWRequest(gamerTag)
+
+        if (Object.keys(wz).length !== 0) {
+          // will insert null rows
+          if (wz.gamerTag.toLowerCase() !== gamerTag.toLowerCase()) {
+            logger.error(`[COD API] Data was not for this user: ${gamerTag}`)
+            return null
           }
-          return bulk.push({
-            gamerTag: gamerTag,
-            ...wz.br_all,
-          })
+
+          logger.info(`[COD API] Got data for: ${gamerTag}`)
+          return wz
         }
       } catch (error) {
         logger.error(`[COD API/Database Error] for ${gamerTag}`, error)
       }
     }),
   )
+
+  // bulk insert into database
   if (bulk.length > 0) {
     await db.br_all.bulkCreate(bulk)
     logger.info(`[COD API] Inserted data`)
